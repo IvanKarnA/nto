9e3076352f398f56253056e91193386c1fc17e78
Ivan K <ivan03.05.2005@gmail.com>
2023-03-22 21:06:03 +0400

    add lcdPrint

--- lab/lab.ino
+++ lab/lab.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <iostream>
+#include <I2C_graphical_LCD_display.h>
 #include "Adafruit_APDS9960.h"
 #include "SparkFun_SGP30_Arduino_Library.h"
 #include "mcp3021.h"
@@ -7,6 +8,7 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include <MPU6050.h>
+
 #define I2C_HUB_ADDR        0x70
 #define EN_MASK             0x08
 #define DEF_CHANNEL         0x00
@@ -16,6 +18,7 @@
 #define GP14 0x05
 #define GP5 0x04
 #define GP18 0x03
+I2C_graphical_LCD_display lcd;
 uint16_t clear;
 MCP3021 mcp3021;
 SGP30 CO30;
@@ -41,11 +44,12 @@ void setup(){
   StartAll();
 }
 void loop(){
-  std::cout<< getTemperature()<<"  "<< getHumidity()<<"  "<< getPressure()<<"\n";
+  
   delay(100);
 }
 void StartAll(){
   Wire.begin();
+  lcd.begin();
   mcp3021.begin(WaterID);
  CO30.begin();
 CO30.initAirQuality();
@@ -54,6 +58,10 @@ bme280.begin();
 LightSensor_1.setMode(Continuously_High_Resolution_Mode);
 }
 
+
+void lcdPrint(String s){
+  lcd.string( s.c_str(),false);
+}
 float getTemperature(){
   return bme280.readTemperature();
 }
